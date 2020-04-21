8d2e91f171d43fd079f4722e78304e8d1c3c8799
divyaaveerama <47803361+divyaaveerama@users.noreply.github.com>
2020-04-21 16:27:01 -0700

    Update otxprint.py

--- otxprint.py
+++ otxprint.py
@@ -1,9 +1,8 @@
 import re
 from OTXv2 import OTXv2
 from OTXv2 import IndicatorTypes
-otx = OTXv2("39cf879fe132e36e50547ea5d9fe5911c3ce099d4ef7680a42c29cf84153a740")
-otxlist = ["5e9a305bf4a514345e851c22","5e9b81b20fa89499c3492c5b","5e9b89d2dd401d48efca8815","5e9b9360c9f1ecea115ecc22","5e9b9363705f075c45ca8815","5e9b936449addd4c29492c5b","5e9b9362e91d1f3f53e2743b","5e9b93ce1db0f133f152f14e"]
-
+otx = OTXv2("") #put your A.P.I key in there
+otxlist = ["",""] #put the pulse IDs in here
 #Create lists for each type of IOC
 IPs = []
 URLs = []
@@ -22,8 +21,9 @@ def getIndicators(pulse_id):
     indicators = otx.get_pulse_indicators(pulse_id)
     for indicator in indicators:
         print(indicator["indicator"])
-    
-def getIndicatorsfromList():
+
+        
+def getIndicatorsfromList(): #use this if you have multiple pulses to scrape
     for pulse_id in otxlist:
     
         indicators = otx.get_pulse_indicators(pulse_id)
@@ -62,7 +62,7 @@ def getIndicatorsfromList():
     print("\n")    
             
     
-def printListToFile(filename):
+def printListToFile(filename): #ignore this for now
     fo = open(filename,"a+")
     for indicator in indicators:
         fo.write(indicator["indicator"])
@@ -71,5 +71,5 @@ def printListToFile(filename):
 if __name__ == "__main__":
     import sys
     getIndicatorsfromList()
-    #printListToFile(str(sys.argv[1]))
+    #getIndicators(str(sys.argv[1])) #use this if you only have 1 pulse to scrape
     
